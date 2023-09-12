a00492a9ae09cea5e9250be73b6bd764c5d6f026
Yashaswini Kondakindi <109444074+yashaswinikbr@users.noreply.github.com>
2023-09-12 08:38:01 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -107,7 +107,7 @@ const Footer = () => {
           rel="nofollow noopener noreferrer">
           <div>
             Designed &amp; Built by Brittany Chiang<br></br>
-            Revised by Chandrika Deb
+            Revised by Yashaswini Kondakindi
           </div>
 
           {githubInfo.stars && githubInfo.forks && (
